1db30ac539b07716269f46f583c099489026637c
Paulinagabriella <143114461+Paulinagabriella@users.noreply.github.com>
2023-09-20 16:07:05 -0500

    Update hackme.php

--- hackme.php
+++ hackme.php
@@ -6,26 +6,10 @@ if ($authorized == 1)
 {
 
     echo "You are currently logged in <br> <br>";
-//NEW IF CONDITION for logged in users
-    $secret = $_POST['flag_value'];
-
-    if ($secret == 'blog')
-    {
-
-       echo "The flag is Flag-Success123";
-
-    }
-    else
-    {
-
-       echo "This webpage accepts a POST value of 'flag_value'. Use raft-small-words to brute-force this parameter and find the flag!";
-       echo "If you want to log out click <a href='logout.php'> here </a> to log out";
-
-    }
+    echo "If you want to log out click <a href='logout.php'> here </a> to log out";
 
 }
-if ($authorized != 1)
-
+else
 {
 
        echo "You are not logged in. Please go to <a href='index.php'> this </a> to log in";
