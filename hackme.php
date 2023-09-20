5ed409802f8a8edbf1cbd204cc3e80f5e9b1ccab
Paulinagabriella <143114461+Paulinagabriella@users.noreply.github.com>
2023-09-20 16:01:55 -0500

    This is my lab11 commit

--- hackme.php
+++ hackme.php
@@ -3,33 +3,17 @@ session_start();
 $authorized = $_SESSION['authorized'];
 
 if ($authorized == 1)
-{
+    {
 
     echo "You are currently logged in <br> <br>";
-//NEW IF CONDITION for logged in users
-    $secret = $_POST['flag_value'];
-
-    if ($secret == 'blog')
-    {
-
-       echo "The flag is Flag-Success123";
+    echo "If you want to log out click <a href='logout.php'> here </a> to log out";
 
     }
-    else
+    else 
     {
 
-       echo "This webpage accepts a POST value of 'flag_value'. Use raft-small-words to brute-force this parameter and find the flag!";
-       echo "If you want to log out click <a href='logout.php'> here </a> to log out";
+    echo "You are not logged in. Please go to <a href='index.php'> this </a> to log in";
 
     }
-
-}
-if ($authorized != 1)
-
-{
-
-       echo "You are not logged in. Please go to <a href='index.php'> this </a> to log in";
-
-}
 
 ?>
